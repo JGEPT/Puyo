b5af5b4f1801b831dc36679fbb66d13a3b2d4a0a
JGEPT <jamesty016@gmail.com>
2023-06-03 17:17:40 +0800

    Added some transitions and designs

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const envelope = document.querySelector(".envelope");
 const letter = document.querySelector(".letter");
 const prev = document.querySelector(".carousel-button.prev");
 const next = document.querySelector(".carousel-button.next");
+const James = document.querySelector(".bgletter")
+const PensButton = document.querySelector(".take");
+const CloseButton = document.querySelector(".close");
 
 LetterButton.addEventListener("click", () => {
     wrapper.classList.toggle('active');
@@ -15,6 +18,24 @@ LetterButton.addEventListener("click", () => {
     lid_one.classList.toggle('active');
     prev.classList.toggle('active');
     next.classList.toggle('active');
+    James.classList.toggle('active');
+    LetterButton.classList.toggle('active');
+    PensButton.classList.toggle('active');
+    CloseButton.classList.toggle('active');
+})
+
+CloseButton.addEventListener("click", () => {
+  wrapper.classList.toggle('active');
+  envelope.classList.toggle('active');
+  letter.classList.toggle('active');
+  lid_two.classList.toggle('active');
+  lid_one.classList.toggle('active');
+  prev.classList.toggle('active');
+  next.classList.toggle('active');
+  James.classList.toggle('active');
+  LetterButton.classList.toggle('active');
+  PensButton.classList.toggle('active');
+  CloseButton.classList.toggle('active');
 })
 
 const buttons = document.querySelectorAll("[data-carousel-button]")
