50e78e17e1dc9b930c55631cb04045c046e9c039
James Gabriel Elijah P. Ty <74401356+JGEPT@users.noreply.github.com>
2023-07-23 17:52:59 +0800

    Implemented fetching data for message list

--- src/index.js
+++ src/index.js
@@ -1,40 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { 
-  getFirestore,
-  collection,
-  getDocs
-} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC5NpXn6fMDQzUjiTIBebUn_7r9hcaqpuY",
-  authDomain: "puyo-cbb18.firebaseapp.com",
-  projectId: "puyo-cbb18",
-  storageBucket: "puyo-cbb18.appspot.com",
-  messagingSenderId: "1016111692864",
-  appId: "1:1016111692864:web:caf62e1b638c971a0eb3a5",
-  measurementId: "G-HRKQ0C8GFL"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-const db = getFirestore();
-const messageref = collection(db, 'MessageSpyke')
-
-getDocs(messageref).then((snapshot) =>{
-  let MessageData = []
-  snapshot.docs.forEach((doc) => {
-    MessageData.push ({...doc.data(), id: doc.id})
-  })
-  console.log(MessageData)
-}) .catch(err => {
-  console.log(err.message);
-})
-
 const LetterButton = document.querySelector(".open");
 const prev = document.querySelector(".carousel-button.prev");
 const next = document.querySelector(".carousel-button.next");
@@ -64,10 +27,37 @@ const PassPrompt = document.querySelector(".passprompt");
 const passform = document.getElementById("passform");
 const MessageList = document.querySelector(".messagelist");
 const readmessage = document.querySelector(".readmessage");
+const info = document.querySelector(".info");
 let music = document.getElementById("player");
 let slider = document.getElementById("slider");
 let number = document.getElementById("number");
 let password;
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { 
+  getFirestore,
+  collection,
+  getDocs
+} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5NpXn6fMDQzUjiTIBebUn_7r9hcaqpuY",
+  authDomain: "puyo-cbb18.firebaseapp.com",
+  projectId: "puyo-cbb18",
+  storageBucket: "puyo-cbb18.appspot.com",
+  messagingSenderId: "1016111692864",
+  appId: "1:1016111692864:web:caf62e1b638c971a0eb3a5",
+  measurementId: "G-HRKQ0C8GFL"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 
 LetterButton.addEventListener("click", () => {
@@ -178,66 +168,241 @@ James.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "Surely";
   owner = "James";
-})
+
+  const messagejamesref = collection(db, 'MessageJames')
+
+  getDocs(messagejamesref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
+  })
 
 Aira.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test1";
   owner = "Aira";
+
+  const messageairaref = collection(db, 'MessageAira')
+
+  getDocs(messageairaref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Summi.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test2";
   owner = "Summi";
+
+  const messagesummiref = collection(db, 'MessageSummi')
+
+  getDocs(messagesummiref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Spyke.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test3";
   owner = "Spyke";
+  const messagespykeref = collection(db, 'MessageSpyke')
+
+getDocs(messagespykeref).then((snapshot) =>{
+  const MessageData = snapshot.docs.map((doc) => {
+    const data = doc.data();
+
+    return `<div class="datacol">
+    <p class="sender">${data.From}</p>
+    <p class="Date">${data.Date}</p>
+  </div>`
+  })
+  info.innerHTML = MessageData.join("\n");
+}).catch(err => {
+  console.log(err.message);
+})
 })
 
 Steph.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Steph";
+
+  const messagestephref = collection(db, 'MessageSteph')
+
+  getDocs(messagestephref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Edgiza.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
-  owner = "Steph";
+  owner = "Edg";
+
+  const messageedgref = collection(db, 'MessageEdg')
+
+  getDocs(messageedgref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Raymond.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Raymond";
+
+  const messageraymondref = collection(db, 'MessageRaymond')
+
+  getDocs(messageraymondref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Andre.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Andre";
+
+  const messageandreref = collection(db, 'MessageAndre')
+
+  getDocs(messageandreref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Kent.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Kent";
+
+  const messagekentref = collection(db, 'MessageKent')
+
+  getDocs(messagekentref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Desiree.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Desiree";
+
+  const messagedesireeref = collection(db, 'MessageDesiree')
+
+  getDocs(messagedesireeref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 Hya.addEventListener("click", () => {
   PassPrompt.classList.toggle('active');
   password = "test";
   owner = "Hya";
+
+  const messagehyaref = collection(db, 'MessageHya')
+
+  getDocs(messagehyaref).then((snapshot) =>{
+    const MessageData = snapshot.docs.map((doc) => {
+      const data = doc.data();
+  
+      return `<div class="datacol">
+      <p class="sender">${data.From}</p>
+      <p class="Date">${data.Date}</p>
+    </div>`
+    })
+    info.innerHTML = MessageData.join("\n");
+  }).catch(err => {
+    console.log(err.message);
+  })
 })
 
 function submitForm(event){
@@ -249,16 +414,14 @@ function submitForm(event){
        }
   if(document.getElementById('password').value === password);{
          MessageList.classList.add("active");
+         const LetterData = document.querySelector(".datacol");
+         LetterData.addEventListener("click", () => {
+          readmessage.classList.add('active');
+         })
        }
    }
 
 passform.addEventListener('submit', submitForm);
-
-const LetterData = document.querySelector(".datacol");
-
-LetterData.addEventListener("click", () => {
-  readmessage.classList.add('active');
-})
 
 const backbutton = document.querySelector(".back");
 
